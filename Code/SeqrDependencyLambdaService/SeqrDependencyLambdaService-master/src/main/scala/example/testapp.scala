49e5edbc89eb4a8388bb1167416103f9dca2fddb
Joyce <zhao.xue1@husky.neu.edu>
2019-10-31 14:45:58 -0400

    adding chr parameter

--- Code/SeqrDependencyLambdaService/SeqrDependencyLambdaService-master/src/main/scala/example/testapp.scala
+++ Code/SeqrDependencyLambdaService/SeqrDependencyLambdaService-master/src/main/scala/example/testapp.scala
@@ -3,17 +3,19 @@ package example
 import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
 
 class ScalaLambda extends RequestHandler[String, String] {
-  override def handleRequest(event: String, context: Context): String = {
+    override def handleRequest(event: String, context: Context): String = {
 
-    // input is: chr=1,min=100,max=500,service=Wuxi
-    val args = event.split(",")
+        // input format: chr,min,max,service
+        // input is:     "1,100,500,Wuxi"
+        val args = event.split(",")
 
+        if (args(3).equalsIgnoreCase("WUXI")) {
+            // query format: gor -p chr1:111371-563625
+            return "gor -p chr" + args(0) + ":" + args(1) + "-" + args(2) + " #dbsnp#"
+        } else if (args(3).equalsIgnoreCase("HAIL")) {
+            return "Using Hail"
+        }
 
-    if (args(2).equalsIgnoreCase("WUXI")) {
-        return "gor -p chr1:" + args(0) + "-" + args(1)
-    } else if (args(2).equalsIgnoreCase("HAIL")) {
-        return "Using Hail"
-    }
-    return "Error Service"
+        return "Error Service"
     }
 }
