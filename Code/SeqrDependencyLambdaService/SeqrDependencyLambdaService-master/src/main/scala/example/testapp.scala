2fb4614403c06b9772dd8c9bfd1270e613abb3d0
Rong Su <su.r@husky.neu.edu>
2019-10-31 18:43:23 -0400

    simple get request to ES

--- Code/SeqrDependencyLambdaService/SeqrDependencyLambdaService-master/src/main/scala/example/testapp.scala
+++ Code/SeqrDependencyLambdaService/SeqrDependencyLambdaService-master/src/main/scala/example/testapp.scala
@@ -2,17 +2,18 @@ package example
 
 import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
 
+
 class ScalaLambda extends RequestHandler[String, String] {
   override def handleRequest(event: String, context: Context): String = {
 
     // input is: chr=1,min=100,max=500,service=Wuxi
     val args = event.split(",")
 
-
     if (args(2).equalsIgnoreCase("WUXI")) {
         return "Using Wuxi"
     } else if (args(2).equalsIgnoreCase("HAIL")) {
-        return "Using Hail"
+        val content = scala.io.Source.fromURL("https://search-test2-hnkwuh5gdzna4fwynnbelmggkq.us-east-2.es.amazonaws.com/_search").mkString
+        return content
     }
     return "Error Service"
     }
