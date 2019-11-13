12abf5828c6e9ac4689c04ec3b90793af8f7a97f
Phoenix <doshi.ja@husky.neu.edu>
2019-11-13 13:16:03 -0500

    Updating code structure

--- Code/SeqrDependencyLambdaService/SeqrDependencyLambdaService-master/src/main/scala/example/testapp.scala
+++ Code/SeqrDependencyLambdaService/SeqrDependencyLambdaService-master/src/main/scala/example/testapp.scala
@@ -6,74 +6,76 @@ import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder
 import com.amazonaws.services.secretsmanager.model._
 
 class ScalaLambda extends RequestHandler[String, String] {
-    override def handleRequest(event: String, context: Context): String = {
+  override def handleRequest(event: String, context: Context): String = {
 
-        // AWS Secrets Manager
-        val secretName: String = "TestSecret"
-        val region: String = "us-east-2"
-        var secret: String = null
+    // AWS Secrets Manager
 
-        val client = AWSSecretsManagerClientBuilder.standard().withRegion(region).build()
+    // input format: chr,min,max,service
+    // input is:     "1,100,500,Wuxi"  or  "1,100,500,Hail"
+    val args = event.split(",")
 
-        val getSecretValueRequest: GetSecretValueRequest = new GetSecretValueRequest().withSecretId(secretName)
-        var getSecretValueResult: GetSecretValueResult = null
+    if (args(3).equalsIgnoreCase("WUXI")) {
+      val secretName: String = "TestSecret"
+      val region: String = "us-east-2"
+      var secret: String = null
 
-        try{
-          getSecretValueResult = client.getSecretValue(getSecretValueRequest)
+      val client = AWSSecretsManagerClientBuilder.standard().withRegion(region).build()
 
-        } catch {
-          case e: InternalServiceErrorException =>
-            throw e
-          case e: DecryptionFailureException =>
-            throw e
-          case e: InvalidParameterException =>
-            throw e
-          case e: InvalidRequestException =>
-            throw e
-          case e: ResourceNotFoundException =>
-            throw e
-        }
+      val getSecretValueRequest: GetSecretValueRequest = new GetSecretValueRequest().withSecretId(secretName)
 
-        if (getSecretValueResult.getSecretString() != null){
-          secret = getSecretValueResult.getSecretString()
-        }
-        else{
-          secret = "Not working"
-        }
+      var getSecretValueResult: GetSecretValueResult = null
 
-        return secret
-        // input format: chr,min,max,service
-        // input is:     "1,100,500,Wuxi"  or  "1,100,500,Hail"
-        val args = event.split(",")
+      try {
+        getSecretValueResult = client.getSecretValue(getSecretValueRequest)
 
-        if (args(3).equalsIgnoreCase("WUXI")) {
-            // query format: gor -p chr1:111371-563625
-            return "gor -p chr" + args(0) + ":" + args(1) + "-" + args(2) + " #dbsnp#"
-        } else if (args(3).equalsIgnoreCase("HAIL")) {
-          /*GET test1/_search
-            {
-              "query": {
-                "bool" : {
-                  "must" : [
-                    {"match" : {"locus.contig" : "1"}},
-                    {"range" :
-                      {
-                        "locus.position" : {
-                        "gte" : 900000,
-                        "lt" : 1000000
-                        }
-                      }
+      } catch {
+        case e: InternalServiceErrorException =>
+          throw e
+        case e: DecryptionFailureException =>
+          throw e
+        case e: InvalidParameterException =>
+          throw e
+        case e: InvalidRequestException =>
+          throw e
+        case e: ResourceNotFoundException =>
+          throw e
+      }
+
+      if (getSecretValueResult.getSecretString() != null) {
+        secret = getSecretValueResult.getSecretString()
+      }
+      else {
+        secret = "Not working"
+      }
+
+      // query format: gor -p chr1:111371-563625
+      return "gor -p chr" + args(0) + ":" + args(1) + "-" + args(2) + " #dbsnp#"
+
+    } else if (args(3).equalsIgnoreCase("HAIL")) {
+      /*GET test1/_search
+        {
+          "query": {
+            "bool" : {
+              "must" : [
+                {"match" : {"locus.contig" : "1"}},
+                {"range" :
+                  {
+                    "locus.position" : {
+                    "gte" : 900000,
+                    "lt" : 1000000
                     }
-                  ]
+                  }
                 }
-              }
-            }*/
+              ]
+            }
+          }
+        }*/
 
-          val url = "https://search-test2-hnkwuh5gdzna4fwynnbelmggkq.us-east-2.es.amazonaws.com/_search?q=locus.contig:" +
-                    args(0) + "%20AND%20locus.position:[" + args(1) + "+TO+" + args(2) + "]&pretty"
-          val content = scala.io.Source.fromURL(url).mkString
-          return content
-        }
-        return "Error Service"
+      val url = "https://search-test2-hnkwuh5gdzna4fwynnbelmggkq.us-east-2.es.amazonaws.com/_search?q=locus.contig:" +
+        args(0) + "%20AND%20locus.position:[" + args(1) + "+TO+" + args(2) + "]&pretty"
+      val content = scala.io.Source.fromURL(url).mkString
+      return content
     }
+    return "Error Service"
+  }
 }
