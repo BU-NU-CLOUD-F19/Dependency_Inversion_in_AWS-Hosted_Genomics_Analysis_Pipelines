b45f1132d017c2ccda818052e5f14d3a8c6ff478
Phoenix <doshi.ja@husky.neu.edu>
2019-11-13 12:27:27 -0500

    Adding SecretManager code

--- Code/SeqrDependencyLambdaService/SeqrDependencyLambdaService-master/build.sbt
+++ Code/SeqrDependencyLambdaService/SeqrDependencyLambdaService-master/build.sbt
@@ -1,12 +1,13 @@
 name := "aws-lambda-scala"
 
-scalaVersion := "2.13.0"
+scalaVersion := "2.11.8"
 
 scalacOptions in ThisBuild ++= Seq("-unchecked", "-deprecation")
-
 
 libraryDependencies += "com.amazonaws" % "aws-lambda-java-core" % "1.2.0"
 
 libraryDependencies += "com.amazonaws" % "aws-lambda-java-events" % "2.2.2"
 
 libraryDependencies += "org.elasticsearch.client" % "elasticsearch-rest-client" % "7.4.2"
+
+libraryDependencies += "com.amazonaws" % "aws-java-sdk-secretsmanager" % "1.11.442"
--- Code/SeqrDependencyLambdaService/SeqrDependencyLambdaService-master/src/main/scala/example/testapp.scala
+++ Code/SeqrDependencyLambdaService/SeqrDependencyLambdaService-master/src/main/scala/example/testapp.scala
@@ -1,11 +1,47 @@
 package example
 
-import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
 
+import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
+import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder
+import com.amazonaws.services.secretsmanager.model._
 
 class ScalaLambda extends RequestHandler[String, String] {
     override def handleRequest(event: String, context: Context): String = {
 
+        // AWS Secrets Manager
+        val secretName: String = "TestSecret"
+        val region: String = "us-east-2"
+        var secret: String = null
+
+        val client = AWSSecretsManagerClientBuilder.standard().withRegion(region).build()
+
+        val getSecretValueRequest: GetSecretValueRequest = new GetSecretValueRequest().withSecretId(secretName)
+        var getSecretValueResult: GetSecretValueResult = null
+
+        try{
+          getSecretValueResult = client.getSecretValue(getSecretValueRequest)
+
+        } catch {
+          case e: InternalServiceErrorException =>
+            throw e
+          case e: DecryptionFailureException =>
+            throw e
+          case e: InvalidParameterException =>
+            throw e
+          case e: InvalidRequestException =>
+            throw e
+          case e: ResourceNotFoundException =>
+            throw e
+        }
+
+        if (getSecretValueResult.getSecretString() != null){
+          secret = getSecretValueResult.getSecretString()
+        }
+        else{
+          secret = "Not working"
+        }
+
+        return secret
         // input format: chr,min,max,service
         // input is:     "1,100,500,Wuxi"  or  "1,100,500,Hail"
         val args = event.split(",")
