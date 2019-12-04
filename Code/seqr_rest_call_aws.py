b27d7571c91b6b11860f6b79d60c1ce40684aec3
Rong Su <su.r@husky.neu.edu>
2019-12-04 09:09:30 -0500

    simple python post request to api gateway

--- Code/seqr_rest_call_aws.py
+++ Code/seqr_rest_call_aws.py
@@ -1,7 +1,7 @@
 import requests
 
-data="1,10,100,wuxi"
-headers={'auth': 'allow'}
-r = requests.post("https://hq0om13an6.execute-api.us-east-2.amazonaws.com/test", headers=headers, data=data)
+data='\"1,10,100,wuxi\"' #do not escape quotes
+headers={'Content-Type': 'application/x-amz-json-1.0', 'x-amz-date': '20191204T04:42:05Z', 'Host': '2mck5641ia.execute-api.us-east-2.amazonaws.com'}
+r = requests.post("https://2mck5641ia.execute-api.us-east-2.amazonaws.com/test-deploy-su/seqr-backend-service", headers=headers, data=data)
 
 print(r.text)
