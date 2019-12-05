62c7c334279d3a80b631db31686f346a4d7f27c5
Nicholas LeCompte <nicholas.lecompte@childrens.harvard.edu>
2019-12-04 20:57:18 -0500

    direct query API continued, still need to finish wiring a few things in

--- Code/SeqrDependencyLambdaService/src/main/scala/wuxi/GorConnection.scala
+++ Code/SeqrDependencyLambdaService/src/main/scala/wuxi/GorConnection.scala
@@ -6,9 +6,9 @@ import requests._
 import java.util.Date
 import scala.util.Try
 
-class GorConnection(api_key: String, project: String, application_url: String, api_endpoint : String) {
+object GorConnection {
 
-    def get_jwt_token_payload(jwt_token: String) : Either[Map[String,Any],Exception] = {
+    def get_jwt_token_payload(jwt_token: String) : Either[Map[String,String],Exception] = {
         var parts = jwt_token.split(".")
         if (parts.length != 3) {
             Right(new IllegalArgumentException("Not a valid JWT token"))
@@ -18,18 +18,24 @@ class GorConnection(api_key: String, project: String, application_url: String, a
             var payload_raw = parts(1)
     //        var payload = java.util.Base64.getDecoder.decode(payload_raw)
             var signature = parts(2)
-            val jsonMap = JSON.parseFull(payload_raw)
-            jsonMap match {
-                case Some(jsonmapping : Map[String,Any]) => Left(jsonmapping)
+            val jsonMapRaw = JSON.parseFull(payload_raw)
+            jsonMapRaw match {
+                case Some(mapping : Any) => Left(mapping.asInstanceOf[Map[String,String]])
                 case None => Right(new Exception("Unspecifed error parsing json"))
-                case Some(_) => Right(new Exception("Error parsing JSON"))
             } //.values.asInstanceOf[Map[String, Any]]
 
         }
     }
+}
+
+case class GorConnection(api_key: String, project: String, application_url: String) {
+
+    var authorizationHeader : Map[String,String] = Map()
+
+    var endpoints : Map[String,String] = Map()
 
     def get_access_token(access_endpoint_url : String) : Either[Map[String,String],Exception] =  {
-        var tokenparts = get_jwt_token_payload(api_key)
+        var tokenparts = GorConnection.get_jwt_token_payload(api_key)
         tokenparts match {
             case Right(error) => Right(error)
             case Left(jsonMapping) =>
@@ -54,7 +60,8 @@ class GorConnection(api_key: String, project: String, application_url: String, a
                     var try_json_response = JSON.parseFull(response.text)
                     try_json_response match {
                         case None => Right(new Exception("Malformed response in get_access_token, could not parse as JSON"))
-                        case Some(mapping : Map[String,Any]) => {
+                        case Some(mappingraw : Any) => {
+                            val mapping = mappingraw.asInstanceOf[Map[String,String]]
                             val try_tokentype = mapping get "token_type"
                             try_tokentype match {
                                 case None => Right(new Exception("Malformed response JSON in get_access_token, did not have token_type field"))
@@ -68,49 +75,42 @@ class GorConnection(api_key: String, project: String, application_url: String, a
                                         case Some(_) => Right(new Exception("Malformed response JSON in get_access_token, access token was not a string"))
                                 }
                             }
-                                case Some(_) => {
-                                    Right(new Exception("Malformed response JSON in get_access_token, token_type was not a string"))
-                                }
                         }
                     }
-                        case Some(_) => {
-                                    Right(new Exception("Malformed response JSON in get_access_token, token_type was not a string"))
-                        }
                 }
         }
     }}
 
-    def invoke_api_request(method : String, header: Map[String,String]) : Either[String, Exception] = {
+    def merge[K, V](m1:Map[K, V], m2:Map[K, V]):Map[K, V] = {
+      (m1.keySet ++ m2.keySet) map { i => i -> (m1.get(i).toList ::: m2.get(i).toList).head } toMap
+    }
+
+    def invoke_api_request(method : String, params: Map[String,String], url: String) : Either[Response, Exception] = {
         val resp = method match {
             case "GET" => {
-                Left(requests.get(application_url + api_endpoint, params=header))
+                Left(requests.get(url, params=params, headers=authorizationHeader))
             }
             case "PUT" => {
-                Left(requests.put(application_url + api_endpoint, params=header))
+                Left(requests.put(url, params=params, headers=authorizationHeader))
             }
             case "POST" => {
-                Left(requests.post(application_url + api_endpoint, params=header))
+                Left(requests.post(url, params=params, headers=authorizationHeader))
             }
             case _ => {Right(new Exception("API method " + method + " not supported"))}
         }
-        resp match {
-            case Right(e) => Right(e)
-            case Left(r) => {
-                Left(r.text)
-            }
-        }
+        resp
     // requests.put(url, )
     }
 
-
     def connect() : Either[Map[String,String],Exception] = {
-        var token_payload = get_jwt_token_payload(api_key)
+        var token_payload = GorConnection.get_jwt_token_payload(api_key)
         token_payload match {
-            case Left(mapping: Map[String,String]) => {
-                var expiry_date_str = mapping get "exp"
+            case Left(token_mapping: Map[String,String]) => {
+                var expiry_date_str = token_mapping get "exp"
                 expiry_date_str match {
-                    case Some(datestr) => {
+                    case Some(datestr : String) => {
                         var tryepoch = Try(datestr.toInt).toOption
+
                         var epoch = {
                             tryepoch match {
                                 case Some(epochint:Int) => epochint
@@ -123,7 +123,7 @@ class GorConnection(api_key: String, project: String, application_url: String, a
                             Right(new Exception("JWT token was expired"))
                         }
                         else {
-                            var tryurl = mapping get "iss"
+                            var tryurl = token_mapping get "iss"
                             tryurl match {
                                 case None => Right(new Exception("Malformed JWT token did not have ISS field"))
                                 case Some(url: String) => {
@@ -131,16 +131,52 @@ class GorConnection(api_key: String, project: String, application_url: String, a
                                     tryheader match {
                                         case Right(e: Exception) => Right(e)
                                         case Left(header : Map[String,String]) => {
+                                            authorizationHeader = header
                                             val try_access_token_payload = header get "authorization"
                                             try_access_token_payload match {
                                                 case None => Right(new Exception("Malformed get_access_token return dict did not have authorization field"))
                                                 case Some(payload: String) => {
-                                                    val response = invoke_api_request("GET",header)
-                                                    response match {
-                                                        case Left(m) => {
-                                                            Left(Map("OK" -> "ok"))
+                                                    val payloadMapRaw = JSON.parseFull(payload)
+                                                    payloadMapRaw match {
+                                                        case None => Right(new Exception("Payload in get_access_token could not be parsed as JSON"))
+                                                        case Some(payload_mapping: Any) => {
+                                                            val payloadMap = payload_mapping.asInstanceOf[Map[String,String]]
+                                                            val tryServiceRoot = payloadMap get "service_root"
+                                                            tryServiceRoot match {
+                                                                case None => Right(new Exception("Could not get service_root from access token"))
+                                                                case Some(root: String) => {
+                                                                    val connDataNoHeader : Map[String,String] = Map(
+                                                                        "api_key" -> api_key,
+                                                                        "service_root" -> root,
+                                                                        "project" -> project,
+                                                                        "api_key_exp" -> ((token_mapping get "exp") match {
+                                                                            case Some(exp:String) => exp
+                                                                            case None => ""
+                                                                        }),
+                                                                        "api_key_iat" -> ((token_mapping get "iat") match {
+                                                                            case Some(iat:String) => iat
+                                                                            case None => ""
+                                                                        }),
+                                                                        "access_token_exp" -> ((payloadMap get "exp") match {
+                                                                            case Some(exp:String) => exp
+                                                                            case None => ""
+                                                                        }),
+                                                                        "access_token_iat" -> ((payloadMap get "iat") match {
+                                                                            case Some(iat:String) => iat
+                                                                            case None => ""
+                                                                        })
+                                                                    )
+                                                                    val connData = merge(connDataNoHeader, header)
+                                                                    var connectResponse = invoke_api_request("GET",connData,root)
+                                                                    connectResponse match {
+                                                                        case Left(m) => {
+                                                                            Left(Map("OK" -> "ok"))
+                                                                        }
+                                                                        case Right(e: Exception) => Right(e)
+                                                                    }
+                                                                }
+                                                            }
                                                         }
-                                                        case Right(e) => Right(e)
                                                     }
                                                 }
 
--- Code/SeqrDependencyLambdaService/src/main/scala/wuxi/GorQuery.scala
+++ Code/SeqrDependencyLambdaService/src/main/scala/wuxi/GorQuery.scala
@@ -1,10 +1,66 @@
 package SeqrDependencyService.wuxi
 
-class GorQuery() {
+import scala.util.parsing.json._
 
-    var QueryString : String = ""
+class GorQuery(query_string: String, connection: GorConnection) {
 
-    def execute_query(connection: GorConnection) {
+    var query_url : String = ""
+
+    def get_query_status() : Either[String,Exception] = {
+        if (query_url == "") {
+            Left("NOT YET SUBMITTED")
+        }
+        else {
+            var response = connection.invoke_api_request("GET", Map(), query_url)
+            response match {
+                case Left(data) => {
+                    if (data.text contains "FAILED") {
+                        Left("FAILED")
+                    }
+                    else {
+                        if (data.text contains "DONE") {
+                            Left("DONE")
+                        }
+                        else {
+                            Left("IN PROGRESS")
+                        }
+                    }
+                }
+                case Right(e: Exception) => Right(e)
+            }
+
+        }
+    }
+
+    def get_query_results() {
+        val result_url = query_url + "offset=0&limit=10000"
+        var tryresponse = connection.invoke_api_request("GET",Map(), result_url)
+        tryresponse match {
+            case Left(response) => {
+                var response_map_raw = JSON.parseFull(response.text)
+                response_map_raw match {
+                    case None => Right(new Exception("Could not parse JSON result"))
+                    case Some(o: Any) => {
+                        val response_map = o.asInstanceOf[Map[String,String]]
+                        Left(response_map)
+                }
+            }
+        }
+            case Right(e) => Right(e)
+
+        }
+    }
+
+    def execute_query() : Either[String, Exception] = {
+        val body = Map("query" -> query_string, "project" -> connection.project)
+        val query_endpoint = (connection.endpoints) get "query"
+        query_endpoint match {
+            case Some(endpoint) => {
+                var query_response = connection.invoke_api_request("POST",body,connection.application_url + query_endpoint)
+                Left("OK")
+            }
+            case None => Right(new Exception("query was not listed in the API endpoints"))
+        }
 
     }
 }
