30f37316ea966c919fa7674082355d6287785743
Rong Su <su.r@husky.neu.edu>
2019-12-05 20:43:09 -0500

    error handling

--- Code/seqr_rest_call_aws.py
+++ Code/seqr_rest_call_aws.py
@@ -1,6 +1,20 @@
 import requests
+import sys
 
-data='\"1,10,100,wuxi\"' #do not escape quotes
+if len(sys.argv) != 5:
+    print("This script should take 4 arguments: chromosome, min, max and service")
+    exit()
+
+if sys.argv[4].lower() not in ["hail", "wuxi"]:
+    print("service selected should be hail or wuxi")
+    exit()
+
+if not (sys.argv[1].isdigit() and sys.argv[2].isdigit() and sys.argv[3].isdigit()):
+    print("chromosome, min and max should be number format")
+    exit()
+
+data='\"' + sys.argv[1] + ',' + sys.argv[2] + ',' + sys.argv[3] + ',' + sys.argv[4] + '\"' #do not escape quotes
+print(data)
 headers={'Content-Type': 'application/x-amz-json-1.0', 'x-amz-date': '20191204T04:42:05Z', 'Host': '2mck5641ia.execute-api.us-east-2.amazonaws.com'}
 r = requests.post("https://2mck5641ia.execute-api.us-east-2.amazonaws.com/test-deploy-su/seqr-backend-service", headers=headers, data=data)
 
